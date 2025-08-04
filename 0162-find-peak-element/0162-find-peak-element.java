df99a0bf9f0aa3eef1a926ed41bc79fdd8c5942c
shubhamgupta9311 <shubhamgupta7642@gmail.com>
2025-08-04 16:12:28 +0530

    Time: 0 ms (100%), Space: 42.2 MB (94.92%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+     if(nums.length==1){
+        return 0;  //case when  no of element is only 1
+     }
+     if(nums[0]>nums[1]) {
+        return 0;  //left most corner case
+     }
+     if(nums[nums.length-1]>nums[nums.length-2]){
+
+        return nums.length-1;  //right most corner case
+     }
+
+      int start=1;
+      int end=nums.length-2;
+      while(start<=end){
+        int mid=(start+end)/2;
+        if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+            return mid;
+        }
+        else if(nums[mid]>nums[mid-1]){
+            start=mid+1;
+        }
+        else{
+            end=mid-1;
+        }
+      }
+
+
+  return -1;
+               
+    }
+}
