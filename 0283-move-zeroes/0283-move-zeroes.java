c81bb7d60dfd2077826b804b2a0f4ec86627b848
shubhamgupta9311 <shubhamgupta7642@gmail.com>
2025-08-13 07:42:28 +0530

    Time: 3 ms (21.41%), Space: 45.9 MB (85.71%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,18 +1,21 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int left=0;//use to set the position of non zero numbers
+        int left = 0;//use to set the position of non zero numbers
         int right;//use to iterate over all array element for check it is non zero or zero numbers.
-        for(right=0;right<nums.length;right++){
-             if(nums[right]!=0){
-                int temp=nums[right];      //swapping
-                nums[right]=nums[left];
-                nums[left]=temp;
+        for (right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                if (left != right) {  //skip swapping when both index are same 
+                    int temp = nums[right]; //swapping
+                    nums[right] = nums[left];
+                    nums[left] = temp;
+                  
+                }
                 left++;
-             }
+            }
             //  else{
             //     //skip nothing to do
             //  }
-        } 
-    
+        }
+
     }
 }
