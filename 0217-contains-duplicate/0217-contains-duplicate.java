2d69174fda2b68e2aab2d2ccf09518d0c5a8042a
shubhamgupta9311 <shubhamgupta7642@gmail.com>
2025-08-21 12:09:09 +0530

    Time: 19 ms (36.44%), Space: 59.7 MB (22.28%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,17 +1,32 @@
 class Solution {
-    public boolean containsDuplicate(int[] nums) {
-    int n=nums.length;
-    int si=0;
-    int ci=n;
-    Arrays.sort(nums,si,ci);
-    int l=0;
-    for(int r=1;r<n;r++){
-         if(nums[l]==nums[r]){
-            return true;
-         }
-       l++;
-    }
-   return false;
+    public boolean containsDuplicate(int[] nums) { 
+        //using inbulit sort but tc goes O(nlogn)
+//     int n=nums.length;
+//     int si=0;
+//     int ci=n;
+//     Arrays.sort(nums,si,ci);
+//     int l=0;
+//     for(int r=1;r<n;r++){
+//          if(nums[l]==nums[r]){
+//             return true;
+//          }
+//        l++;
+//     }
+//    return false;
         
+//     }
+
+//one way of doing using hashset
+    
+    int n=nums.length;
+    HashSet<Integer> set=new HashSet<>();
+    for(int num:nums){
+        set.add(num);
+    }
+   return set.size()<n;
+
+
+
+
     }
 }
