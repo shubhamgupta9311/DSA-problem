e9fce72a57433449bd2fa64185c3cf416129c76f
Shubham Gupta <shubhamgupta7642@gmail.com>
2026-03-24 22:54:34 +0530

    Time: 1 ms (10.23%), Space: 48.1 MB (43.34%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,19 +1,29 @@
 class Solution {
     public double myPow(double x, int n) {
-        double prod = 1;
-        //when n is negative
-        if (n < 0) {
-            for (int j = 0; j > n; j--) {
-                prod = prod * 1 / x;
-            }
-return prod;
-        }
-        //when n is positive
-        for (int i = 0; i < n; i++) {
-            prod = prod * x;
-        }
-        return prod;
+        //1.method-done my me brute force but iT GIVES TLE as n is very big here
+//         double prod = 1;
 
-    }
+//         //when n is negative
+//         if (n < 0) {
+//             for (int j = 0; j > n; j--) {
+//                 prod = prod * 1 / x;
+//             }
+// return prod;
+//         }
+//         //when n is positive
+//         for (int i = 0; i < n; i++) {
+//             prod = prod * x;
+//         }
+//         return prod;
+
+//     }
     
+
+//2. using inbult function,but not recommeded in interview although it is optimal
+
+return Math.pow(x,n);
+
+
+
+}
 }
