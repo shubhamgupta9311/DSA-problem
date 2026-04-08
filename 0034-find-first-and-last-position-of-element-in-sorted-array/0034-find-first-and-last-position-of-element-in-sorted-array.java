964e99b975766615db378d97ccfa12fce5258910
Shubham Gupta <shubhamgupta7642@gmail.com>
2026-04-09 00:45:39 +0530

    Time: 0 ms (100%), Space: 48.1 MB (77.88%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,22 +1,57 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
 
-        int[] arr = new int[2];
-        arr[0] = arr[1] = -1; //if e=we dont find that target in array
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                arr[0] = i;
-                while (i+1 < nums.length && nums[i + 1] == target) {
-                    i++;
-                   
-                   
-                }
-                  arr[1] = i;
-            
+        if(nums.length==0){
+            return new int[]{-1,-1};
+        }
+        int low = 0;
+        int high = nums.length - 1;
+        return new int[]{Fo(nums,target,low,high),Lo(nums,target,low,high)};
+
+    }
+
+    public int Fo(int nums[], int target, int low, int high ) {
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] >= target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
 
         }
-        return arr;
+         if(low>nums.length-1){
+            low--;
+         }
+        if (nums[low] == target) {
+            return low;
+        }
+        return -1;
 
+    }
+
+    public int Lo(int nums[], int target, int low, int high) {
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] <= target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+
+        }
+
+         if(high<0){
+            high=0;
+         }
+
+        if (nums[high] == target) {
+            return high;
+        }
+        return -1;
     }
 }
