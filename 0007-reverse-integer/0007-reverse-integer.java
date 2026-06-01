9da119fbe9e14e0dd768f65f54f109733cf3a7ea
Shubham Gupta <shubhamgupta7642@gmail.com>
2026-06-01 12:33:51 +0530

    Time: 1 ms (99.98%), Space: 42.5 MB (78.7%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,11 +1,29 @@
 class Solution {
     public int reverse(int x) {
-  int sign=(x<0)? -1:1;
-  x=Math.abs(x);
+//   int sign=(x<0)? -1:1;
+//   x=Math.abs(x);
+//   long rev=0;
+//   while(x>0){
+// rev=rev*10 +x%10;
+// x=x/10;
+
+//   }
+//   long res=rev*sign;
+
+//   if(res<Integer.MIN_VALUE ||res>Integer.MAX_VALUE){
+//     return 0;
+//   }
+//   return (int)res;
+        
+//a hidden test case that will give error to Math.abs when x is integer.min value it will not  added in leetcode test cases 
+
+
+        int sign=(x<0)? -1:1;
+  long num =Math.abs((long)x);
   long rev=0;
-  while(x>0){
-rev=rev*10 +x%10;
-x=x/10;
+  while(num>0){
+rev=rev*10 +num%10;
+num=num/10;
 
   }
   long res=rev*sign;
@@ -14,6 +32,5 @@ x=x/10;
     return 0;
   }
   return (int)res;
-        
     }
 }
