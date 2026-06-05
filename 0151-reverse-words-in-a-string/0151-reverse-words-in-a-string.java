3cc4ab85692cabeeb86745f93907da6bde5034b0
Shubham Gupta <shubhamgupta7642@gmail.com>
2026-06-06 02:48:27 +0530

    Time: 6 ms (82.8%), Space: 44.6 MB (32.91%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,14 +1,26 @@
 class Solution {
     public String reverseWords(String s) {
         String arr[]=s.trim().split("\\s+");
-        String ans="";
-        for(int i=arr.length-1;i>=0;i--){
-        ans+=arr[i];
-        if(i!=0){
-            ans+=" ";
-        }
-        }
+        // String ans="";
+//         for(int i=arr.length-1;i>=0;i--){
+//         ans+=arr[i];
+//         if(i!=0){
+//             ans+=" ";
+//         }
+//         }
+// return ans;
 
-return ans;
+
+//using stringbuilder-more optimized
+StringBuilder ans=new StringBuilder();
+
+for(int i=arr.length-1;i>=0;i--){
+ans.append(arr[i]);
+if(i!=0){
+    ans.append(" ");
+}
+}
+return ans.toString();    //here i got the error as i write only ans ans now ans is stringbulider not a string
+
     }
 }
