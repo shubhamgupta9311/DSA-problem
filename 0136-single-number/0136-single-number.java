565c97dfeee08efb88345c0bc01f0c4e3bd7ae4f
Shubham Gupta <shubhamgupta7642@gmail.com>
2026-06-17 16:20:51 +0530

    Time: 535 ms (5.02%), Space: 46.8 MB (80.73%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,21 +1,53 @@
 class Solution {
+    //brute force
+    
     public int singleNumber(int[] nums) {
-      
-          for(int i=0;i<nums.length;i++){
-            int count=0;
-             for ( int j=0;j<nums.length;j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
-              
-               }
-                if(count==1){
-                return nums[i];
-                }
-               
+        // int n=nums.length;
+        // int count=0;
+        // for(int i=0;i<n;i++){
+        //     count=0;
+        //     for(int j=0;j<n;j++){
+        //         if(nums[i]==nums[j]){
+        //             count++;
+        //         }
+        //     }
+        //     if(count==1){
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
 
-             }
-  return -1;
-          }
-       
+
+
+
+//again visiting 2026
+//trying first brute force
+int n=nums.length;
+for(int i=0;i<n;i++){
+     int count=0;
+     for(int j=0;j<n;j++){
+        if(nums[i]==nums[j]){
+            count++;
+        }
+     }
+        if(count==1){
+            return nums[i];
+        }
+}
+
+return -1;
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+}
