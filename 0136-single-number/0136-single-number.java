622910728480995fcf6590b671a3883f475f297a
Shubham Gupta <shubhamgupta7642@gmail.com>
2026-06-17 16:59:57 +0530

    Time: 1 ms (99.97%), Space: 46.8 MB (63.9%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -22,23 +22,30 @@ class Solution {
 
 //again visiting 2026
 //trying first brute force
-int n=nums.length;
-for(int i=0;i<n;i++){
-     int count=0;
-     for(int j=0;j<n;j++){
-        if(nums[i]==nums[j]){
-            count++;
-        }
-     }
-        if(count==1){
-            return nums[i];
-        }
+// int n=nums.length;
+// for(int i=0;i<n;i++){
+//      int count=0;
+//      for(int j=0;j<n;j++){
+//         if(nums[i]==nums[j]){
+//             count++;
+//         }
+//      }
+//         if(count==1){
+//             return nums[i];
+//         }
+// }
+
+// return -1;
+
+
+//optimization
+//xor gate(bit manipulation)
+int ans=0;
+for(int num:nums){
+    ans^=num;
 }
 
-return -1;
-
-
-
+return ans;
 
 
 
