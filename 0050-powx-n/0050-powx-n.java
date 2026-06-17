570f9612a54abb85cbf2670c11335684a328609b
Shubham Gupta <shubhamgupta7642@gmail.com>
2026-06-17 18:30:21 +0530

    Time: 0 ms (100%), Space: 48.1 MB (46.15%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -21,9 +21,59 @@ class Solution {
 
 //2. using inbult function,but not recommeded in interview although it is optimal
 
-return Math.pow(x,n);
+// return Math.pow(x,n);
+
+//3/optimal approach of solution
+// if(n==0) return 1.0;
+// if(x==0) return 0.0;
+// if(x==1) return 1.0;
+// if(x==-1 && n%2==0) return 1.0;
+// if(x==-1 && n%2!=0) return -1.0;
+
+// long binform=n;
+// if(n<0){
+//     x=1/x;
+//     binform=-binform;
+// }
+
+// double ans=1;
+// while(binform>0){
+//     if(binform%2==1){
+//         ans*=x;
+//     }
+//     x*=x;
+//     binform/=2;
+// }
+// return ans;
 
 
+//4.doing it in 2026 using fast exopentiation
+if(n==0) return 1.0;
+if(x==0) return 0.0;
+if(x==1) return 1.0;
+if(x==-1 && n%2==0) return 1.0;
+if(x==-1 && n%2!=0) return -1.0;
 
+long N=n;
+if(N< 0){
+    x = 1/x;
+    N=-N;
 }
+double ans = 1;
+double pow = x;
+
+
+while(N> 0){
+
+    if((N & 1) == 1){
+        ans *= pow;
+    }
+
+    pow *= pow;
+    N >>= 1;
 }
+
+return ans;
+
+    }
+    }
