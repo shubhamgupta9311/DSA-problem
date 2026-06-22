96e8c2fddd51a13291bf7aa9286eab00cbec0857
Shubham Gupta <shubhamgupta7642@gmail.com>
2026-06-23 04:25:30 +0530

    Time: 0 ms (100%), Space: 47 MB (8.78%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,26 +1,19 @@
-class Solution { //here we have to return only k ,judge can auto read values of new updated array
-    //* we dont have to store underscore also as judge autmatically see it 
-    //instruction-It does not matter what you leave beyond the returned k (hence they are underscores).
+class Solution { 
     public int removeDuplicates(int[] nums) {
-        int n = nums.length;
-        int l = 0; //left index
-        int r; //right index
-        for (r = 1; r < n; r++) {
-            //  if(nums[l]==nums[r]){    //no requirement
-            //       nums[r]=
-            //  } 
-            if (nums[l] != nums[r]) {
-                l++;
-                if (l != r) {
-                //     int temp = nums[r];
-                //     nums[r] = nums[l];
-                //     nums[l] = temp;      //no need as we dont have care about rest element ,with value of k the judge automatucally found that how much elemnt i have to focus
-                // }
-                nums[l]=nums[r];}
 
-            }
+//doing on 22/6/26
+ int lp=0;
+ int rp;
+ for(rp=0;rp<nums.length;rp++){
+    if(nums[lp]!=nums[rp]){
+        lp++;
+        nums[lp]=nums[rp];
+    }
 
-        }
-        return l + 1;
+ }
+
+ return lp+1;
+
+
     }
 }
