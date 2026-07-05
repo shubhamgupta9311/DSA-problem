ed83215f3b67b27b69fc1faca8bad1d109e2625a
Shubham Gupta <shubhamgupta7642@gmail.com>
2026-07-05 12:33:11 +0530

    Time: 10 ms (25.93%), Space: 42 MB (69.6%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,22 +1,36 @@
 class Solution {     //not may be optimized
     public int fib(int n) {
-        int firstTerm=0;
-        int secondTerm=1;
-        if(n==0){
-            return 0;
-        }
-        else if(n==1){
-            return 1;
-        }
-        else{
-              for(int i=1;i<=n;i++){
+        // int firstTerm=0;
+        // int secondTerm=1;
+        // if(n==0){
+        //     return 0;
+        // }
+        // else if(n==1){
+        //     return 1;
+        // }
+        // else{
+        //       for(int i=1;i<=n;i++){
             
-                int thirdTerm=firstTerm+secondTerm;
-                firstTerm=secondTerm;
-                secondTerm=thirdTerm;
-              }
-              return firstTerm;
-        }
+        //         int thirdTerm=firstTerm+secondTerm;
+        //         firstTerm=secondTerm;
+        //         secondTerm=thirdTerm;
+        //       }
+        //       return firstTerm;
+        // }
 
+
+
+
+
+//doing using recursion
+
+
+
+//base case
+if(n==0 ||n==1){
+    return n;
+}
+
+return (fib(n-1)+fib(n-2));
     }
 }
