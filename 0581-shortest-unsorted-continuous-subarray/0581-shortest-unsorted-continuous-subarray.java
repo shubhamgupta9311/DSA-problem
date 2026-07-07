e184ebad300ad3f32f6fb379543bbf2692b5f7ec
Shubham Gupta <shubhamgupta7642@gmail.com>
2026-07-07 11:25:36 +0530

    Time: 1 ms (90.83%), Space: 47.5 MB (31.41%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.java
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.java
@@ -1,50 +1,102 @@
 class Solution {
     public int findUnsortedSubarray(int[] nums) {
 
-if(nums.length==1){
-    return 0;
-}
-int fbk=-1;
-int lbk=-1;
-int min=Integer.MAX_VALUE;
-int max=Integer.MIN_VALUE;
-//first break point
- for(int i=0;i<nums.length-1;i++){
+// if(nums.length==1){
+//     return 0;
+// }
+// int fbk=-1;
+// int lbk=-1;
+// int min=Integer.MAX_VALUE;
+// int max=Integer.MIN_VALUE;
+// //first break point
+//  for(int i=0;i<nums.length-1;i++){
+// if(nums[i]>nums[i+1]){
+//    fbk=i;
+//    break;
+// }
+//  }
+//  if(fbk==-1){
+//     return 0;
+//  }
+
+
+
+//  //last break point
+//   for(int i=nums.length-1;i>0;i--){
+// if(nums[i]<nums[i-1] ){
+//    lbk=i;
+ 
+//    break;
+// }
+//  }
+//  //finding min and max in current range
+//  for(int i=fbk; i<=lbk;i++){
+//     min=Math.min(min,nums[i]);
+//     max=Math.max(max,nums[i]);
+//  } 
+
+//  //left expansion,checking is there any elemnt in left exits that is greater than min if it then include in range
+//  while(fbk>0 && nums[fbk-1]>min){
+//     fbk--;
+//  }
+//  //right expansion,checking is there any element in right exits which is smaller then max if it then include in range
+//  while(lbk<nums.length-1 && nums[lbk+1]<max){
+//     lbk++;
+//  }
+
+//   return (lbk-fbk)+1;
+
+
+//revision code
+
+//finding fbk and lbk
+  int fbk=-1;
+  int lbk=-1;
+  int min=Integer.MAX_VALUE;
+  int max=Integer.MIN_VALUE;
+for(int i=0;i<nums.length-1;i++){
+
 if(nums[i]>nums[i+1]){
-   fbk=i;
-   break;
+fbk=i;
+break;
 }
- }
- if(fbk==-1){
+
+}
+
+if(fbk==-1){
     return 0;
- }
-
-
-
- //last break point
-  for(int i=nums.length-1;i>0;i--){
-if(nums[i]<nums[i-1] ){
-   lbk=i;
- 
-   break;
 }
- }
- 
- for(int i=fbk; i<=lbk;i++){
-    min=Math.min(min,nums[i]);
-    max=Math.max(max,nums[i]);
- } 
+for(int i=nums.length-1;i>0;i--){
 
- //left expansion
- while(fbk>0 && nums[fbk-1]>min){
-    fbk--;
- }
- //right expansion
- while(lbk<nums.length-1 && nums[lbk+1]<max){
-    lbk++;
- }
+if(nums[i]<nums[i-1]){
+lbk=i;
+break;
+}
 
-  return (lbk-fbk)+1;
+}
+
+
+//minimum and maximum
+for(int i=fbk;i<=lbk ;i++){
+min=Math.min(min,nums[i]);
+max=Math.max(max,nums[i]);
+}
+
+//left side expansion
+for(int i=fbk-1;i>=0;i--){
+    if(nums[i]>min){
+        fbk--;
+    }
+}
+//right side expasion
+for(int i=lbk+1;i<=nums.length-1;i++){
+    if(nums[i]<max){
+        lbk++;
+    }
+}
+
+
+return (lbk-fbk)+1;
 
     }
 }
