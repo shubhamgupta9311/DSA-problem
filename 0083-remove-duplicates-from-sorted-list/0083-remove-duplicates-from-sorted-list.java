80817a207657d8321fe9259748ff150feccba60c
Shubham Gupta <shubhamgupta7642@gmail.com>
2026-07-10 13:21:57 +0530

    Time: 0 ms (100%), Space: 45.4 MB (50.62%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode Dummy=new ListNode(-1);
+        Dummy.next=head;
+        ListNode temp=head;
+if(head==null){
+    return head;
+}
+
+        while(head.next!=null){
+if(head.val!=head.next.val){
+  temp.next=head.next;
+         temp=head.next;
+}
+
+      head=head.next;
+        }
+        temp.next=null;
+
+         return Dummy.next;
+    }
+   
+}
