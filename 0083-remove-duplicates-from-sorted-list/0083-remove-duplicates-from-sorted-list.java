baef8a9dcee7d5a71242bbe09bc7a6c55cef71db
Shubham Gupta <shubhamgupta7642@gmail.com>
2026-07-17 09:02:39 +0530

    Time: 0 ms (100%), Space: 45.5 MB (32.61%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -10,24 +10,51 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        ListNode Dummy=new ListNode(-1);
-        Dummy.next=head;
-        ListNode temp=head;
+//         ListNode Dummy=new ListNode(-1);
+//         Dummy.next=head;
+//         ListNode temp=head;
+// if(head==null){
+//     return head;
+// }
+
+//         while(head.next!=null){
+// if(head.val!=head.next.val){
+//   temp.next=head.next;
+//          temp=head.next;
+// }
+
+//       head=head.next;
+//         }
+//         temp.next=null;
+
+//          return Dummy.next;
+
+
+
+
+
 if(head==null){
-    return head;
+return head;
 }
+ListNode temp=head;
+ListNode Next=temp.next;
 
-        while(head.next!=null){
-if(head.val!=head.next.val){
-  temp.next=head.next;
-         temp=head.next;
+
+while(Next!=null){
+
+
+if(temp.val!=Next.val){
+temp.next=Next;
+temp=Next;
 }
+  
+Next=Next.next;
 
-      head=head.next;
-        }
-        temp.next=null;
+}
+temp.next=null;
 
-         return Dummy.next;
+
+return head;
     }
    
 }
