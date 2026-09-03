70bb860f2f4d4d4b8e0e3298dd772dbd1d388203
Shubham Gupta <shubhamgupta7642@gmail.com>
2026-09-03 17:14:26 +0530

    Time: 1 ms (98.18%), Space: 46.8 MB (94.42%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,34 +1,30 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
 
+List<int[]> result =new ArrayList<>();
 
-        List<int[]> result = new ArrayList<>();
+for(int i=0;i<intervals.length;i++){
 
-        for (int i = 0; i < intervals.length; i++) {
+//current interval is completely before new
+if(intervals[i][1]<newInterval[0]){
+    result.add(intervals[i]);
+}
+//current interval is completely after new
+else if(intervals[i][0]>newInterval[1]){
+    result.add(newInterval);
+    newInterval=intervals[i];
+}
+//overlapping
+else{
+newInterval[0]=Math.min(intervals[i][0],newInterval[0]);
+newInterval[1]=Math.max(intervals[i][1],newInterval[1]);
+}
 
-            // 1. Current interval is BEFORE newInterval
-            if (intervals[i][1] < newInterval[0]) {
-                result.add(intervals[i]);
-            }
+}
 
-            // 2. Current interval is AFTER newInterval
-            else if (intervals[i][0] > newInterval[1]) {
-                result.add(newInterval);
-                newInterval = intervals[i];
-            }
+result.add(newInterval);
+ return result.toArray(new int[result.size()][]);
 
-            // 3. Current interval OVERLAPS newInterval
-            else {
-                newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-                newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
-            }
-        }
-
-        // Add the final newInterval
-        result.add(newInterval);
-
-        return result.toArray(new int[result.size()][]);
-
-
+  
     }
 }
