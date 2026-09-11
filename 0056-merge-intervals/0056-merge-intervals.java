40c05207e023af209809eaccdca937a1aea6edbe
Shubham Gupta <shubhamgupta7642@gmail.com>
2026-09-11 06:51:07 +0530

    Time: 8 ms (90.25%), Space: 49 MB (60.07%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,28 +1,58 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-    //1.sort by start
-    Arrays.sort(intervals,(a,b)->(a[0]-b[0]));
-    //2. creating a list
-    List<int[]> result=new ArrayList<>();
-//3.current element
-int [] current=intervals[0];
+//     //1.sort by start
+//     Arrays.sort(intervals,(a,b)->(a[0]-b[0]));
+//     //2. creating a list
+//     List<int[]> result=new ArrayList<>();
+// //3.current element
+// int [] current=intervals[0];
 
-//4.logic
+// //4.logic
+// for(int i=1;i<intervals.length;i++){
+//     int next[]=intervals[i];
+
+//     if(current[1]>=next[0]){
+//         //merge
+//         current[1]=Math.max(current[1],next[1]);
+
+//     }
+//     else{
+//         result.add(current);
+//         current=next;
+//     }
+// }
+
+// result.add(current);
+
+// return result.toArray(new int[result.size()][]);
+
+
+//revision 1
+//1.sorting by start
+Arrays.sort(intervals,(a,b)->(a[0]-b[0]));
+
+//2.
+List<int[]> result=new ArrayList<>();
+
+//3.
+int []current=intervals[0];
+
 for(int i=1;i<intervals.length;i++){
-    int next[]=intervals[i];
+int[] next=intervals[i];
 
-    if(current[1]>=next[0]){
-        //merge
-        current[1]=Math.max(current[1],next[1]);
-
-    }
-    else{
-        result.add(current);
-        current=next;
-    }
+if(current[1]>=next[0]){
+current[1]=Math.max(current[1],next[1]);
+}
+else{
+result.add(current);
+current=next;
 }
 
+}
+
+
 result.add(current);
+
 
 return result.toArray(new int[result.size()][]);
 
