5ea58763effe93d290f888e07104f758edc0436b
Shubham Gupta <shubhamgupta7642@gmail.com>
2026-09-11 10:41:14 +0530

    Time: 1 ms (98.04%), Space: 47.1 MB (37.32%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,30 +1,57 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
 
-List<int[]> result =new ArrayList<>();
+// List<int[]> result =new ArrayList<>();
+
+// for(int i=0;i<intervals.length;i++){
+
+// //current interval is completely before new
+// if(intervals[i][1]<newInterval[0]){
+//     result.add(intervals[i]);
+// }
+// //current interval is completely after new
+// else if(intervals[i][0]>newInterval[1]){
+//     result.add(newInterval);
+//     newInterval=intervals[i];
+// }
+// //overlapping
+// else{
+// newInterval[0]=Math.min(intervals[i][0],newInterval[0]);
+// newInterval[1]=Math.max(intervals[i][1],newInterval[1]);
+// }
+
+// }
+
+// result.add(newInterval);
+//  return result.toArray(new int[result.size()][]);
+
+  
+//     }
+
+
+List <int []> result=new ArrayList<>();
 
 for(int i=0;i<intervals.length;i++){
 
-//current interval is completely before new
-if(intervals[i][1]<newInterval[0]){
-    result.add(intervals[i]);
+//new interval is complety before current
+
+if(intervals[i][0]>newInterval[1]){
+result.add(newInterval);
+newInterval=intervals[i];
 }
-//current interval is completely after new
-else if(intervals[i][0]>newInterval[1]){
-    result.add(newInterval);
-    newInterval=intervals[i];
+ //new interval is complelty after current
+else if(intervals[i][1]<newInterval[0]){
+result.add(intervals[i]);
 }
-//overlapping
+//overalapping case
 else{
 newInterval[0]=Math.min(intervals[i][0],newInterval[0]);
 newInterval[1]=Math.max(intervals[i][1],newInterval[1]);
 }
-
 }
 
-result.add(newInterval);
- return result.toArray(new int[result.size()][]);
 
-  
+result.add(newInterval);
+return result.toArray(new int[result.size()][]);
     }
 }
